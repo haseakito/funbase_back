5332f56a14d6c7c7513cdc8501c20133dd6301b6
haseakito <haseakito@gmail.com>
2023-08-22 14:41:55 +0800

    :fire: Removed import

--- schemas/user.py
+++ schemas/user.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel, EmailStr
-from typing import Optional
 
 class UserCreate(BaseModel):
     username: str
@@ -13,6 +12,3 @@ class UserLogin(BaseModel):
 class Token(BaseModel):
     access_token: str
     token_type: str
-
-class TokenData(BaseModel):
-    id: Optional(str)
