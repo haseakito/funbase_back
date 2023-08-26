2337feac7bf89f5da9a87b458cc540e9e46c0cbf
haseakito <haseakito@gmail.com>
2023-08-27 05:03:37 +0800

    :card_file_box: Modified partial types

--- models/partial_types.py
+++ models/partial_types.py
@@ -1,5 +1,8 @@
-from prisma.models import User
+from prisma.models import User, Profile
 
+"""
+User schema
+"""
 # user response schema
 User.create_partial('UserOut', exclude_relational_fields=True, exclude={'password', 'emailVerified'})
 
@@ -14,3 +17,15 @@ User.create_partial('UserCreate', include={'username', 'email', 'password'})
 
 # reset password schema
 User.create_partial('ResetPassword', include={'email', 'password'})
+
+"""
+Profile schema
+"""
+# update profile bio schema
+Profile.create_partial('UpdateBio', include={'bio'})
+
+# update profile profileImage schema
+Profile.create_partial('UpdateProfile', include={'profileImage'})
+
+# update profile coverImage schema
+Profile.create_partial('UpdateCover', include={'coverImage'})
