a8bc7d18a5d70d7e578a1de276de8432eeb4b48a
haseakito <haseakito@gmail.com>
2023-08-27 05:04:05 +0800

    :sparkles: Add email schema

--- schemas/user.py
+++ schemas/user.py
@@ -1,9 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
+from typing import List, Dict, Any 
 
 # schema for input data when getting users
 class GetUsers(BaseModel):
     take: int
     orderBy: str
+
+class EmailSchema(BaseModel):
+    email: List[EmailStr]
+    body: Dict[str, Any]
 
 # schema for a user when logged in
 class Token(BaseModel):
